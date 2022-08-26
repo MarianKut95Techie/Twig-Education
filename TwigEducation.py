d17553bfc70dbf32d308fd00533716242e19a721
MarianKut95Techie <112169264+MarianKut95Techie@users.noreply.github.com>
2022-08-26 11:52:49 +0100

    Add files via upload

---
+++ TwigEducation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from array import *
+arr = array('i', [1,2,3,4,5,6,7,8,9])
+print(arr)
+
+
+# In[7]:
+
+
+print(arr.buffer_info())
+
+
+# In[13]:
+
+
+import numpy as np
+print(np.array_split(range(9), 2))
+
+
+# In[ ]:
+
+
+#So given N=2, with a range of numbers of 9 starting at 0 [0,1,2,3,4,5,6,7,8,9] we have 2 equal splits of new array 
+new array = my_array/N 
+my_array = 9
+N = 2
+new array = 9/2
+4.5
+This is rounded up to 5
+
